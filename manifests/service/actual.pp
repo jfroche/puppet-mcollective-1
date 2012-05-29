9a620f63c1e910b807ce9406ab41496c24bbd048
Jean-François Roche <jfroche@affinitic.be>
2012-05-29 16:39:12 +0200

    add mcollective init.d (do not depend on redhat-lsb)

--- manifests/service/actual.pp
+++ manifests/service/actual.pp
@@ -4,6 +4,16 @@ class mcollective::service::actual {
   include mcollective::config
   include mcollective::plugins
 
+  file {
+    '/etc/init.d/mcollective':
+      ensure => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      source => "puppet:///modules/mcollective/mcollective.init",
+      notify  => Service['mcollective'],
+  }
+
   service { 'mcollective':
     ensure     => 'running',
     hasstatus  => true,
