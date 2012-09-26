752b840638d809ab6ae06958716bed1c3ebb5693
Julien Pivotto <roidelapluie@inuits.eu>
2012-09-26 08:49:47 +0200

    update puppetcommander

---
+++ manifests/puppetcommander.pp
@@ -0,0 +1,30 @@
+class mcollective::puppetcommander {
+  file {
+    '/etc/init.d/puppetcommander':
+      ensure => present,
+      source => 'puppet:///modules/mcollective/plugins/agent/puppetd/commander/puppetcommander.init',
+      mode   => '0755',
+  }
+  file {
+    '/etc/puppetcommander.cfg':
+      ensure => present,
+      source => 'puppet:///modules/mcollective/puppetcommander.cfg',
+  }
+  file {
+    '/etc/sysconfig/puppetcommander':
+      ensure  => present,
+      content => 'MCOLLECTIVE_EXTRA_OPTS=""'
+  }
+  file {
+    '/usr/sbin/puppetcommanderd':
+      ensure => present,
+      source => 'puppet:///modules/mcollective/plugins/agent/puppetd/commander/puppetcommanderd',
+      mode   => '0755',
+  }
+  service {
+    'puppetdcommander':
+      ensure     => running,
+      hasstatus  => true,
+      hasrestart => true,
+  }
+}
